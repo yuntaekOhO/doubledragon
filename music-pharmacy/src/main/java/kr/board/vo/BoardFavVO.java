74e0376fa439b8383e86f06f2f9cd7b611a5f711
godsangsu <96238871+godsangsu@users.noreply.github.com>
2022-07-21 14:27:04 +0900

    BoardFavVO

---
+++ music-pharmacy/src/main/java/kr/board/vo/BoardFavVO.java
@@ -0,0 +1,35 @@
+package kr.board.vo;
+
+public class BoardFavVO {
+	private int fav_num;
+	private int the_num;
+	private int free_num;
+	private int mem_num;
+	
+	public int getFav_num() {
+		return fav_num;
+	}
+	public void setFav_num(int fav_num) {
+		this.fav_num = fav_num;
+	}
+	public int getThe_num() {
+		return the_num;
+	}
+	public void setThe_num(int the_num) {
+		this.the_num = the_num;
+	}
+	public int getFree_num() {
+		return free_num;
+	}
+	public void setFree_num(int free_num) {
+		this.free_num = free_num;
+	}
+	public int getMem_num() {
+		return mem_num;
+	}
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+	
+	
+}
