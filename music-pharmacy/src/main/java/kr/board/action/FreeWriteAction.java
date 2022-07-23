81f88f16b5976d1e99d32b9e571bec4888d712d3
easyp <easyp@DESKTOP-K1N31VV>
2022-07-23 23:44:29 +0900

    지수커밋

---
+++ music-pharmacy/src/main/java/kr/board/action/FreeWriteAction.java
@@ -0,0 +1,40 @@
+package kr.board.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.oreilly.servlet.MultipartRequest;
+
+import kr.board.dao.FreeBoardDAO;
+import kr.board.vo.FreeBoardVO;
+import kr.controller.Action;
+import kr.util.FileUtil;
+
+public class FreeWriteAction implements Action{
+
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		HttpSession session = request.getSession();
+		Integer mem_num = 
+				(Integer)session.getAttribute("mem_num");
+		if(mem_num==null) {
+			return "redirect:/member/loginForm.jsp";
+		}
+		
+		//로그인이 된 경우
+		MultipartRequest multi = FileUtil.createFile(request);
+		FreeBoardVO board = new FreeBoardVO();
+		board.setFree_title(multi.getParameter("free_title"));
+		board.setFree_content(multi.getParameter("free_content"));
+		board.setFree_img(multi.getFilesystemName("free_img"));
+		board.setMem_num(mem_num);
+		
+		FreeBoardDAO dao = FreeBoardDAO.getInstance();
+		dao.insertBoard(board);
+		
+		return "/WEB-INF/views/board/freeWrite.jsp";
+	}
+
+}
