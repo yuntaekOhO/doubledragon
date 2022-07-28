9e9144621e7a6beb8551fdf7c106ae19ddd1921b
yunchaeyeong <yunchaeyeong@localhost>
2022-07-28 17:56:54 +0900

    theme

--- music-pharmacy/src/main/java/kr/board/dao/ThemeBoardDAO.java
+++ music-pharmacy/src/main/java/kr/board/dao/ThemeBoardDAO.java
@@ -787,4 +787,32 @@ public class ThemeBoardDAO {
 				DBUtil.executeClose(null, pstmt, conn);
 			}
 		}
+		
+		//댓글 삭제
+		public void deleteReplyBoard(int treply_num)
+				                    throws Exception{
+			Connection conn = null;
+			PreparedStatement pstmt = null;
+			String sql = null;
+			
+			try {
+				//커넥션풀로부터 커넥션을 할당
+				conn = DBUtil.getConnection();
+				//SQL문 작성
+				sql = "DELETE FROM theme_comment WHERE treply_num=?";
+				//PreparedStatement 객체 생성
+				pstmt = conn.prepareStatement(sql);
+				//?에 데이터 바인딩
+				pstmt.setInt(1, treply_num);
+				//SQL문 실행
+				pstmt.executeUpdate();
+				
+			}catch(Exception e) {
+				throw new Exception(e);
+			}finally {
+				//자원정리
+				DBUtil.executeClose(null, pstmt, conn);
+			}
+		}
+		
 }
