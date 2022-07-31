cc6eec425556123077d20fdd71b7528f93a4f7a0
yuntaekOhO <oyt1996@naver.com>
2022-07-31 19:15:18 +0900

    유저목록

--- music-pharmacy/src/main/java/kr/member/dao/MemberDAO.java
+++ music-pharmacy/src/main/java/kr/member/dao/MemberDAO.java
@@ -3,6 +3,8 @@ package kr.member.dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 import kr.member.vo.MemberVO;
 import kr.util.DBUtil;
@@ -153,6 +155,106 @@ public class MemberDAO {
 			DBUtil.executeClose(rs, pstmt, conn);
 		}
 		return member;
+	}
+	
+	//회원 수
+	public int getMemberCountByAdmin(String keyfield, String keyword)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		String sub_sql = "";
+		int count = 0;
+		
+		try {
+			conn = DBUtil.getConnection();
+			
+			if(keyword!=null && !"".equals(keyword)) {
+				if(keyfield.equals("1")) sub_sql = "WHERE m.id LIKE ?";
+				else if(keyfield.equals("2")) sub_sql = "WHERE d.nick LIKE ?";
+				else if(keyfield.equals("3")) sub_sql = "WHERE m.auth LIKE ?";
+			}
+			
+			sql = "SELECT COUNT(*) FROM member m LEFT OUTER JOIN member_detail d USING (mem_num) " + sub_sql;
+			
+			pstmt = conn.prepareStatement(sql);
+			
+			if(keyword!=null&&!"".equals(keyword)) {
+				pstmt.setString(1, "%"+keyword+"%");
+			}
+			
+			rs = pstmt.executeQuery();
+			if(rs.next()) {
+				count = rs.getInt(1);
+			}
+			
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		return count;
+	}
+	//회원 목록
+	public List<MemberVO> getListMemberByAdmin(int start, int end, String keyfield, String keyword)throws Exception{
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		List<MemberVO> list = null;
+		String sql = null;
+		String sub_sql = "";
+		int cnt = 0;
+		
+		try {
+			conn = DBUtil.getConnection();
+			
+			if(keyword!=null && !"".equals(keyword)) {
+				if(keyfield.equals("1")) sub_sql = "WHERE m.id LIKE ?";
+				else if(keyfield.equals("2")) sub_sql = "WHERE d.nick LIKE ?";
+				else if(keyfield.equals("3")) sub_sql = "WHERE m.auth LIKE ?";
+			}
+			
+			sql = "SELECT * FROM (SELECT a.*, rownum rnum FROM "
+					+ "(SELECT * FROM member m LEFT OUTER JOIN member_detail d USING(mem_num) " + sub_sql
+					+ " ORDER BY mem_num DESC NULLS LAST)a) WHERE rnum>=? AND rnum<=?";
+			
+			pstmt = conn.prepareStatement(sql);
+			if(keyword!=null&&!"".equals(keyword)) {
+				pstmt.setString(++cnt, "%"+keyword+"%");
+			}
+			pstmt.setInt(++cnt, start);
+			pstmt.setInt(++cnt, end);
+			
+			rs = pstmt.executeQuery();
+			
+			list = new ArrayList<MemberVO>();
+			while(rs.next()) {
+				MemberVO member = new MemberVO();
+				member.setMem_num(rs.getInt("mem_num"));
+				member.setId(rs.getString("id"));
+				member.setAuth(rs.getInt("auth"));
+				member.setNick(rs.getString("nick"));
+				member.setName(rs.getString("name"));
+				member.setPasswd(rs.getString("passwd"));
+				member.setCell(rs.getString("cell"));
+				member.setEmail(rs.getString("email"));
+				member.setZipcode(rs.getString("zipcode"));
+				member.setAddr1(rs.getString("addr1"));
+				member.setAddr2(rs.getString("addr2"));
+				member.setReg_date(rs.getDate("reg_date"));
+				member.setMusic(rs.getString("music"));
+				member.setBirthday(rs.getString("birthday"));
+				member.setRoute(rs.getString("route"));
+				member.setPhoto(rs.getString("photo"));
+				list.add(member);
+			}
+		}catch(Exception e) {
+			throw new Exception(e);
+		}finally {
+			DBUtil.executeClose(rs, pstmt, conn);
+		}
+		
+		return list;
 	}
 	
 	//회원정보수정
